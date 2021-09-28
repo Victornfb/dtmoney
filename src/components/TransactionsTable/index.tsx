bc2574acad34cfce1c1a3a5699a612e4e8f3c298
Victor Nunes <victornfb@outlook.com>
2021-09-28 18:07:04 -0300

    Transactions hook

--- src/components/TransactionsTable/index.tsx
+++ src/components/TransactionsTable/index.tsx
@@ -1,9 +1,9 @@
-import { useContext } from "react";
+import { useTransactions } from '../../hooks/useTransactions'
+
 import { Container } from "./styles";
-import { TransactionsContext } from "../../TransactionsContext";
 
 export function TransactionsTable() {
-  const {transactions} = useContext(TransactionsContext);
+  const {transactions} = useTransactions();
 
   return (
     <Container>
@@ -21,10 +21,12 @@ export function TransactionsTable() {
           {transactions.map(transaction => (
             <tr key={transaction.id}>
               <td>{transaction.title}</td>
-              <td className={transaction.type}>{new Intl.NumberFormat('pt-BR', {
-                style: 'currency',
-                currency: 'BRL'
-              }).format(transaction.amount)}</td>
+              <td className={transaction.type}>
+                {new Intl.NumberFormat('pt-BR', {
+                  style: 'currency',
+                  currency: 'BRL'
+                }).format(transaction.amount)}
+              </td>
               <td>{transaction.category}</td>
               <td>{new Intl.DateTimeFormat('pt-BR').format(
                   new Date(transaction.createdAt)
